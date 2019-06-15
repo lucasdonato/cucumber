4981f52f0ab2b303e021eba01869c6d5dd8b651a
Lucas Donato <lucaspolimig96@gmail.com>
2019-06-15 15:36:44 -0300

    alterando alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -19,11 +19,3 @@ apk add --update \
   postgresql-dev \
   curl unzip libexif udev chromium chromium-chromedriver xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
   && rm -rf /var/cache/apk/*
-
-# Tell pry to use `more` as the pager because the installed
-# version of `less` does not support passing the `-R` which
-# is used by pry.
-ENV PAGER="more"
-
-ENV CHROME_BIN=/usr/bin/chromium-browser
-ENV CHROME_PATH=/usr/lib/chromium/
