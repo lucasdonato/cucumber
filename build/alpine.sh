ee92f83160aa9177c99980a0e5452e9734aaf75f
Lucas Donato <lucaspolimig96@gmail.com>
2019-06-15 15:22:01 -0300

    alterando alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -9,13 +9,21 @@ echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositorie
 #    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
 #    && rm -rf /var/cache/apk/*
 
-    
+
 # RUN apk update && \
 #   apk add curl unzip libexif udev chromium chromium-chromedriver xvfb
-apk add --update \
+RUN apk add --update \
   build-base \
   libxml2-dev \
   libxslt-dev \
   postgresql-dev \
   curl unzip libexif udev chromium chromium-chromedriver xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
   && rm -rf /var/cache/apk/*
+
+# Tell pry to use `more` as the pager because the installed
+# version of `less` does not support passing the `-R` which
+# is used by pry.
+ENV PAGER="more"
+
+ENV CHROME_BIN=/usr/bin/chromium-browser
+ENV CHROME_PATH=/usr/lib/chromium/
