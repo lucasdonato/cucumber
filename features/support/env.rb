624e7d8d1c003f0258d7c11113e597e3b357ef90
Lucas Donato <lucaspolimig96@gmail.com>
2019-06-15 23:23:16 -0300

    env

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,7 +13,16 @@ Capybara.configure do |config|
   when 'firefox'
     @driver = :selenium
   when 'chrome_headless'
-    @driver = :selenium_chrome_headless
+    Capybara.register_driver :selenium_chrome_headless do |app|
+      chrome_options =  Selenium::WebDriver::Chrome::Options.new.tap do |options|
+          options.add_argument "--headless"
+          options.add_argument "--disable-gpu"
+          options.add_argument "--no-sandbox"
+          options.add_argument "--disable-site-isolation-trials"
+      end
+      Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+    end
+      @driver = :selenium_chrome_headless    
   when 'firefox_headless'
     @driver = :selenium_headless
   end
