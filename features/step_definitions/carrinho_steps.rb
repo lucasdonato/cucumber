29f7684644d68e6266b29e1b86c2260c1e2426cf
=lucasdonato <=lucaspolimig96@gmail.com>
2019-06-17 16:52:23 -0300

    adicionando log

--- features/step_definitions/carrinho_steps.rb
+++ features/step_definitions/carrinho_steps.rb
@@ -13,6 +13,8 @@ Quando("eu adiciono {int} unidade\\(s)") do |quantidade|
 end
 
 Então("deve ser adicionado {int} unidade\\(s) deste item") do |quantidade|
+ #verificar a saída no jenkins
+  puts "(#{quantidade}x) #{@produto_nome}"
   expect(@rest_page.cart.box).to have_text "(#{quantidade}x) #{@produto_nome}"
 end
 
