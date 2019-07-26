8a8ca7dc8ce23eae40b3aa84604fcd756f25a04d
=lucasdonato <=lucaspolimig96@gmail.com>
2019-07-26 10:54:36 -0300

    adicionando report builder no relatorio

--- Rakefile
+++ Rakefile
@@ -1,11 +1,12 @@
 require "time"
 
 task :specs, [:tags, :browser, :ambiente] do |task, args|
-
   time = Time.now.utc.iso8601.tr(":", "")
   #para rodar o relatorio no jenkins
   #o formato tem que ser json
-  report = "--format json --out=log/report_#{time}.json"
-  sh "cucumber #{args[:tags]} #{report} #{args[:browser]} #{args[:ambiente]}"
+  #  report = "--format json --out=log/report_#{time}.json"
 
+  report = "-f json -o log/report.json -f pretty"
+
+  sh "cucumber #{args[:tags]} #{report} #{args[:browser]} #{args[:ambiente]}"
 end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,6 @@
 require "base64"
+require "report_builder"
+require "date"
 
 Before do
   page.current_window.resize_to(1440, 900)
@@ -18,11 +20,31 @@ end
 
 After do |scenario|
   #if scenario.failed?
-    #shot_file = page.save_screenshot("log/screenshot.png")
-    #shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
-    #embed(shot_b64, "image/png", "Screenshot") # Cucumber anexa o screenshot no report
-  
-    screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
-    embed(screenshot, "image/png", "Screenshot")
-    #end
+  #shot_file = page.save_screenshot("log/screenshot.png")
+  #shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+  #embed(shot_b64, "image/png", "Screenshot") # Cucumber anexa o screenshot no report
+
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
+  #end
+end
+
+at_exit do
+  t = Time.now
+  data_teste = t.strftime("%d/%m/%Y %H:%M:%S")
+  @infos = {
+    "Autor" => "Lucas Donato",
+    "Ambiente" => "Homologação",
+    "Data do Teste" => data_teste,
+  }
+
+  ReportBuilder.configure do |config|
+    config.input_path = "log/report.json"
+    config.report_path = "log/report"
+    config.report_types = [:html]
+    config.report_title = "Enjoeat - Restaurantes"
+    config.additional_info = @infos
+    config.color = "cyan"
+  end
+  ReportBuilder.build_report
 end
